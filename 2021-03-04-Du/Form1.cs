1cbb47837131256682f012f05bd80870a4dc9d84
Pedro <puedroreis@gmail.com>
2021-03-05 17:20:15 -0300

    Update 2021-03-05

--- 2021-03-04-Du/Form1.cs
+++ 2021-03-04-Du/Form1.cs
@@ -20,83 +20,65 @@ namespace _2021_03_04_Du
 
     public partial class Form1 : Form
     {
+        bool aux = true;
         public Form1()
         {
 
-            InitializeComponent();
-            Thread trd2 = new Thread(new ThreadStart(deletarAq));
-            trd2.Start();
+            InitializeComponent();       
 
         }
-
-        private void deletarAq()
-        {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Pedro\Desktop\foto");
-            foreach (FileInfo item in d.GetFiles())
-            {
-                Console.WriteLine(item.Name);
-                //take a dif(time) between last write and datenow
-                TimeSpan t = DateTime.Now.Subtract(item.LastWriteTime);
-                //Dif seconds
-                Console.WriteLine(t.TotalSeconds);
-
-                if (t.TotalSeconds > 50)
-                    item.Delete();
-            }
-        }
-
-
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             Thread trd = new Thread(new ThreadStart(PrintScreen));
-
-            trd.Start();
+            trd.Start();         
 
         }
-
 
         private void PrintScreen()
         {
             //for teste
-            for (int i = 0; i < 20; i++)
+            int j = 0;
+
+           while(aux!= false)
             {
-
-                System.Drawing.Bitmap printscreen = new System.Drawing.Bitmap(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
-                System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(printscreen as System.Drawing.Image);
-                graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
-
-                byte[] byteArray = new byte[0];
-                using (MemoryStream stream = new MemoryStream())
+                if (j ==10)
                 {
-                    printscreen.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                    stream.Close();
-
-                    byteArray = stream.ToArray();
+                    j=0;
                 }
-                DateTime now = DateTime.Now;
-                string asString = now.ToString("dd-MM-yyyy-hh:mm:ss");
 
-                //take screenshot every 5seg
-                Thread.Sleep(5000);
+                j++;
+                
+                    System.Drawing.Bitmap printscreen = new System.Drawing.Bitmap(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+                    System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(printscreen as System.Drawing.Image);
+                    graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
 
-                //open or create file and name as datatime now O.S
-                FileStream fs = new FileStream(@"C:\Users\Pedro\Desktop\foto\" + asString + ".png", FileMode.OpenOrCreate);
+                    byte[] byteArray = new byte[0];
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        printscreen.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                        stream.Close();
 
-                fs.Write(byteArray, 0, byteArray.Length);
-                fs.Close();
+                        byteArray = stream.ToArray();
+                    }
+                   
+                    //take screenshot every 5seg
+                    Thread.Sleep(5000);
 
+                    //open or create file and name as datatime now O.S
+                    FileStream fs = new FileStream(@"C:\Users\Pedro\Desktop\foto\" + j + ".png", FileMode.Create);
 
-
+                    fs.Write(byteArray, 0, byteArray.Length);
+                    fs.Close();
+                
             }
-
-
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            aux = false;
+        }
     }
-
-
 
 
 }
